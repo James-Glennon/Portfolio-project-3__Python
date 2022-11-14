05b50b369dce76b28886f547b358bc03d89070b3
James-Glennon <glennon.jjp91@gmail.com>
2022-11-14 09:41:32 +0000

    Displays current game 'board' from sheets

--- run.py
+++ run.py
@@ -13,6 +13,21 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('battleships_sheet')
 
 player = SHEET.worksheet('Player')
-data = player.get_all_values()
+player_data = player.get_all_values()
+cp = SHEET.worksheet('COM') #cp is short for Computer Player
+cp_data = cp.get_all_values()
+player_guess = SHEET.worksheet('Player_guess') #The player's previous guesses
+player_guess_data = player_guess.get_all_values()
 
-print(data)
+def display_board():
+    """
+    Display the current state of the game board.
+    Keeps computer ship locations hidden.
+    """
+    print("Player's Board")
+    [print(rows) for rows in player_data]
+    print("\n COM's Board")
+    [print(rows) for rows in player_guess_data]
+    print('\nLegend: "-" = blank, "O" = Occupied tile, "X" = Miss, "H" = Hit\n')
+
+display_board()
