eb6e13f43547dea57ac5a585928b5961ee0345c4
James-Glennon <glennon.jjp91@gmail.com>
2022-11-16 04:32:58 +0000

    Remove previous code. Add 2D arrays of guess and hidden board

--- run.py
+++ run.py
@@ -1,33 +1,9 @@
-import gspread
-from google.oauth2.service_account import Credentials
+from random import randint
+from pprint import pprint
 
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
+HIDDEN_BOARD = [[' '] * 8 for x in range(8)]
+GUESS_BOARD = [[' '] * 8 for x in range(8)]
+turns = 20
 
-CREDS = Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('battleships_sheet')
-
-player = SHEET.worksheet('Player')
-player_data = player.get_all_values()
-cp = SHEET.worksheet('COM') #cp is short for Computer Player
-cp_data = cp.get_all_values()
-player_guess = SHEET.worksheet('Player_guess') #The player's previous guesses
-player_guess_data = player_guess.get_all_values()
-
-def display_board():
-    """
-    Display the current state of the game board.
-    Keeps computer ship locations hidden.
-    """
-    print("Player's Board")
-    [print(rows) for rows in player_data]
-    print("\n COM's Board")
-    [print(rows) for rows in player_guess_data]
-    print('\nLegend: "-" = blank, "O" = Occupied tile, "X" = Miss, "H" = Hit\n')
-
-display_board()
+pprint(HIDDEN_BOARD)
+pprint(GUESS_BOARD)
