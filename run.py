71a605bead5035c8891f64a1079495ed1b7838ef
James-Glennon <glennon.jjp91@gmail.com>
2022-11-16 04:42:45 +0000

    Add print_board function

--- run.py
+++ run.py
@@ -1,9 +1,23 @@
 from random import randint
-from pprint import pprint
 
-HIDDEN_BOARD = [[' '] * 8 for x in range(8)]
-GUESS_BOARD = [[' '] * 8 for x in range(8)]
+HIDDEN_BOARD = [[' '] * 8 for x in range(8)]#The grid which maps the ship locations
+GUESS_BOARD = [[' '] * 8 for x in range(8)]#The grid which marks the player's previous guesses
 turns = 20
 
-pprint(HIDDEN_BOARD)
-pprint(GUESS_BOARD)
+def print_board(board):
+    """
+    Displays the guess board in the terminal
+    Adds headings and row numbers
+    """
+    print('  A B C D E F G H') #initial empty spaces in string are to allow letters and columns align
+    print(' ==================') #divider for headers/cells. Purely aesthetic.
+    row_number = 1
+    for row in board:
+        print(f'{row_number}|'+'|'.join(row)+'|')
+        row_number += 1
+
+print('Hidden board')
+print_board(HIDDEN_BOARD)
+
+print('Guess board')
+print_board(GUESS_BOARD)
