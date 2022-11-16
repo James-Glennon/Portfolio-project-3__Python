92a4db6002b345b5e5c4d3f7c9c4004e7b929754
James-Glennon <glennon.jjp91@gmail.com>
2022-11-16 04:51:49 +0000

    add create ships function

--- run.py
+++ run.py
@@ -16,8 +16,26 @@ def print_board(board):
         print(f'{row_number}|'+'|'.join(row)+'|')
         row_number += 1
 
+def create_ships(board):
+    """
+    Creates random locations for 5 single-cell battleships.
+    While loop confirms if the space is unavailable/occupied.
+        if true: another random location is selected.
+        if false: the value 'X' is assigned to the co-ordinate in the hidden board
+    repeats the for loop until all 5 ships are assigned unique locations.
+    """
+    for ship in range(5):
+        ship_row = randint(0,7)
+        ship_column = randint(0,7)
+        while board[ship_row][ship_column] == 'x':
+            ship_row,ship_column = randint(0,7), randint(0,7)
+        board[ship_row][ship_column] = 'x'
+
+
+create_ships(HIDDEN_BOARD)
+
 print('Hidden board')
 print_board(HIDDEN_BOARD)
-
+print('')
 print('Guess board')
 print_board(GUESS_BOARD)
